cf9295cc284d4c9553cf1f09867286c2d54e60ba
John Gilliland <johncgilliland@gmail.com>
2026-05-29 21:59:51 -0700

    fix: Add submitting indicator to new event form using svelte enhance.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,18 @@
 	let { data }: { data: PageData } = $props();
 </script>
 
-<h1 class="text-xl">Events</h1>
-{#await data.events then events}
+<h1 class="text-xl m-4">Events</h1>
+{#await data.events}
+	<p class="text-gray-500 animate-pulse m-8">Loading events…</p>
+{:then events}
 	{#each events as event}
-		<div>
+		<div class="flex flex-col gap-1 max-w-md m-8">
 			<h2 class="text-lg font-bold">{event.id}: {event.title}</h2>
 			<p>{event.description}</p>
 			<p>{event.date}</p>
 		</div>
 	{/each}
+{:catch error}
+	<p class="text-red-500">Failed to load events: {error.message}</p>
 {/await}
-<a class="btn" href="/newevent" role="button">Add Event</a>
+<a class="btn m-4" href="/newevent" role="button">Add Event</a>
--- src/routes/[eventId]/+page.svelte
+++ src/routes/[eventId]/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import type { PageData } from "./$types";
+	import type { PageData } from './$types';
 
-    let {data}: {data: PageData} = $props();
+	let { data }: { data: PageData } = $props();
 </script>
 
-
-<div>
-    {#if data.event}
-        <h2 class="text-lg font-bold">{data.event.id}: {data.event.title}</h2>
-        <p>{data.event.description}</p>
-        <p>{data.event.date}</p>
-    {/if}
+<div class="flex flex-col gap-1 max-w-md m-8">
+	{#if data.event}
+		<h2 class="text-lg font-bold">{data.event.id}: {data.event.title}</h2>
+		<p>{data.event.description}</p>
+		<p>{data.event.date}</p>
+	{/if}
 </div>
+<a class="btn m-4 mt-1" href="/" role="button">All Events</a>
